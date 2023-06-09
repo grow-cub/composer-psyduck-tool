8f4ad26b7fe47d87b19139758a99db78bb24c88e
ta <1057523194@qq.com>
2023-06-09 23:26:50 +0800

    add JwtInterface

--- demo/test.php
+++ demo/test.php
@@ -3,6 +3,13 @@
 
 require_once "../vendor/autoload.php";
 
-use Psyduck\Rsa\RsaInterface;
+use Psyduck\Jwt\JwtInterface;
 
-echo RsaInterface::privateEncrypt(json_encode(array('1'=> 2)));
+$payData = JwtInterface::buildPayload(1);
+echo JwtInterface::getToken($payData);
+JwtInterface::refreshToken(1);
+
+
+//$str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJqd3RfYWRtaW4iLCJpYXQiOjE2ODYzMjAwMTEsImV4cCI6MTY4NjMyMDAyMSwibmJmIjoxNjg2MzIwMDcxLCJzdWIiOjEsImp0aSI6ImJmZjkzNjYyYzgwYjIxYTZhNzM2MDEwNmE0NGM2NTNiIn0.g5AteRpm2GMQQ3bCPtbCxo5RBDbtnEB-Z1X5ifD2rTU';
+//var_dump(JwtInterface::verifyToken($str));
+
