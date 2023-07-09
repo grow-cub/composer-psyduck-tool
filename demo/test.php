86445c51f40fd82e946697175b2f7caa4c3fd3e7
ta <1057523194@qq.com>
2023-07-09 19:51:57 +0800

    检测是否是允许访问的来源网站

--- demo/test.php
+++ demo/test.php
@@ -3,13 +3,18 @@
 
 require_once "../vendor/autoload.php";
 
-use Psyduck\Jwt\JwtInterface;
-
-$payData = JwtInterface::buildPayload(1);
-echo JwtInterface::getToken($payData);
-JwtInterface::refreshToken(1);
+//use Psyduck\Jwt\JwtInterface;
+//
+//$payData = JwtInterface::buildPayload(1);
+//echo JwtInterface::getToken($payData);
+//JwtInterface::refreshToken(1);
 
 
 //$str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJqd3RfYWRtaW4iLCJpYXQiOjE2ODYzMjAwMTEsImV4cCI6MTY4NjMyMDAyMSwibmJmIjoxNjg2MzIwMDcxLCJzdWIiOjEsImp0aSI6ImJmZjkzNjYyYzgwYjIxYTZhNzM2MDEwNmE0NGM2NTNiIn0.g5AteRpm2GMQQ3bCPtbCxo5RBDbtnEB-Z1X5ifD2rTU';
 //var_dump(JwtInterface::verifyToken($str));
+
+use Psyduck\Header\HeaderInterface;
+
+$array = [1,1,2];
+HeaderInterface::dealAllowReferer($array);
 
