f4b1c60e99a4055260f82076534d091055683c96
ta <1057523194@qq.com>
2023-07-30 22:03:40 +0800

    test

--- demo/test.php
+++ demo/test.php
@@ -10,7 +10,7 @@ use Psyduck\Response\ResponseInterface;
 // $data = array();
 // $data['user_name'] = '1';
 // $data['user_age'] = '18';
-// $data['user_school'] = '美团';
+// $data['user_school'] = '美团';1111
 
 // var_dump(ResponseInterface::success($data));
 
@@ -28,4 +28,3 @@ use Psyduck\Header\HeaderInterface;
 
 $array = [1,1,2];
 HeaderInterface::dealAllowReferer($array);
-
